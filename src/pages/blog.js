c9b4776aacc34fb6b7d364edafe62a4de1a964b5
KarolusD <kpodzerek@gmail.com>
2021-09-05 16:03:13 +0200

    fixed packages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,7 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 const BlogPage = () => {
+  console.log('Blog coming soon!')
   return (
     <Layout>
       <Seo title="blog" />
